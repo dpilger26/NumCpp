e355b360ac0b30ab38a6e41817edaff72707a501
David Pilger <dpilger26@gmail.com>
2019-04-18 23:31:26 -0600

    added a laplace filter

--- src/NumCpp/Filter.hpp
+++ src/NumCpp/Filter.hpp
@@ -126,6 +126,10 @@ namespace nc
             Boundary inBoundaryType = Boundary::REFLECT, dtype inConstantValue = 0);
 
         template<typename dtype>
+        NdArray<dtype> laplace(const NdArray<dtype>& inImageArray, Boundary inBoundaryType = Boundary::REFLECT,
+            dtype inConstantValue = 0);
+
+        template<typename dtype>
         NdArray<dtype> maximumFilter(const NdArray<dtype>& inImageArray, uint32 inSize,
             Boundary inBoundaryType = Boundary::REFLECT, dtype inConstantValue = 0);
 
@@ -1048,6 +1052,25 @@ namespace nc
                 inConstantValue).template astype<dtype>();
 
             return std::move(output);
+        }
+
+        //============================================================================
+        // Method Description:
+        ///						Calculate the 2D laplace filter.
+        ///
+        ///                     SciPy Reference: https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.laplace.html#scipy.ndimage.laplace
+        ///
+        /// @param				inImageArray
+        /// @param              inBoundaryType: boundary mode (default Reflect) options (reflect, constant, nearest, mirror, wrap)
+        /// @param				inConstantValue: contant value if boundary = 'constant' (default 0)
+        /// @return
+        ///				NdArray
+        ///
+        template<typename dtype>
+        NdArray<dtype> laplace(const NdArray<dtype>& inImageArray, Boundary inBoundaryType, dtype inConstantValue)
+        {
+            NdArray<dtype> kernel = { {0, 1, 0}, {1, -4, 1}, {0, 1, 0} };
+            return std::move(convolve(inImageArray, 3, kernel, inBoundaryType, inConstantValue));
         }
 
         //============================================================================
