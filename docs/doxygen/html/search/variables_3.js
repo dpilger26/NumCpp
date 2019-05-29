019e46ce8052ba09947fadee4d75ea1a2d35aeb2
David Pilger <dpilger26@gmail.com>
2019-05-28 22:30:37 -0600

    Updated doxygen docs

--- docs/doxygen/html/search/variables_3.js
+++ docs/doxygen/html/search/variables_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['generator_5f',['generator_',['../namespacenc.html#a6af7800546630c07e75fda11de517030',1,'nc']]]
+  ['generator_5f',['generator_',['../namespacenc.html#ac1a8cfb8f9378e1c4da7bc09e93cb77a',1,'nc']]]
 ];
