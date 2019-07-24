429b349f1f84de5269d86185b07162244389c1aa
David Pilger <dpilger26@gmail.com>
2019-07-23 22:50:27 -0600

    added an opencv converting example

---
+++ examples/InterfaceWithOpenCV/Example.cpp
@@ -0,0 +1,44 @@
+#include "NumCpp.hpp"
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <iostream>
+
+int main()
+{
+    // create a random image with NumCpp
+    auto ncArray = nc::random::randInt<nc::uint8>({ 500, 500 }, 0, nc::DtypeInfo<nc::uint8>::max());
+
+    // convert to OpenCV Mat
+    auto cvArray = cv::Mat(ncArray.numRows(), ncArray.numCols(), CV_8SC1, ncArray.data());
+
+    // display the OpenCV Mat
+    cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE); // Create a window for display.
+    cv::imshow("Display window", cvArray); // Show our image inside it.
+    cv::waitKey(0); // Wait for a keystroke in the window
+
+    // tranpose the Mat with OpenCV
+    auto transposedCvArray = cv::Mat(cvArray.cols, cvArray.rows, CV_8SC1);
+    cv::transpose(cvArray, transposedCvArray);
+
+    // display the transposed Mat
+    cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE); // Create a window for display.
+    cv::imshow("Display window", transposedCvArray); // Show our image inside it.
+    cv::waitKey(0); // Wait for a keystroke in the window
+
+    // convert the transposed OpenCV Mat to a NumCpp array
+    auto transposedNcArray = nc::NdArray<nc::uint8>(transposedCvArray.data, transposedCvArray.rows, transposedCvArray.cols);
+
+    // make sure the two transposed arrays are the same
+    if (nc::array_equal(transposedNcArray, ncArray.transpose()))
+    {
+        std::cout << "Arrays are equal." << std::endl;
+    }
+    else
+    {
+        std::cout << "Arrays are not equal." << std::endl;
+    }
+
+    return 0;
+}
