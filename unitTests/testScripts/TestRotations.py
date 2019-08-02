fixed quaternion to dcm

unitTests/testScripts/TestRotations.py
@@ -8,8 +8,16 @@ else:
     sys.path.append(r'../build/x64/Release')
 import NumCpp
 
+
 ####################################################################################
 def doTest():
+    testQuaternion()
+    testDcm()
+    testFunctions()
+
+
+####################################################################################
+def testQuaternion():
     print(colored('Testing Rotations Module', 'magenta'))
 
     print(colored('Testing Quaternion', 'magenta'))
@@ -333,6 +341,9 @@ def doTest():
     else:
         print(colored('\tFAIL', 'red'))
 
+
+####################################################################################
+def testDcm():
     print(colored('Testing DCM', 'magenta'))
 
     print(colored('Testing angleAxisRotationNdArray', 'cyan'))
@@ -391,6 +402,9 @@ def doTest():
     else:
         print(colored('\tFAIL', 'red'))
 
+
+####################################################################################
+def testFunctions():
     print(colored('Testing Functions', 'magenta'))
 
     print(colored('Testing rodriques rotation', 'cyan'))
@@ -407,9 +421,11 @@ def doTest():
     else:
         print(colored('\tFAIL', 'red'))
 
+
 ########################################################################################################################
 def quatNorm(quat):
     return np.linalg.norm(quat)
+
 
 ########################################################################################################################
 def dcm2quat(dcm):
@@ -424,6 +440,7 @@ def dcm2quat(dcm):
     quat = quat / np.linalg.norm(quat)
 
     return quat
+
 
 ########################################################################################################################
 def quat2dcm(quat):
@@ -447,15 +464,18 @@ def quat2dcm(quat):
 
     return dcm
 
+
 ########################################################################################################################
 def quatAdd(quat1, quat2):
     quat = quat1 / quatNorm(quat1) + quat2 / quatNorm(quat2)
     return quat / np.linalg.norm(quat)
 
+
 ########################################################################################################################
 def quatSub(quat1, quat2):
     quat = quat1 / quatNorm(quat1) - quat2 / quatNorm(quat2)
     return quat / np.linalg.norm(quat)
+
 
 ########################################################################################################################
 def quatMult(quat1, quat2):
@@ -467,14 +487,16 @@ def quatMult(quat1, quat2):
     q2 = quat2N[3] * quat1N[2] - quat2N[0] * quat1N[1] + quat2N[1] * quat1N[0] + quat2N[2] * quat1N[3]
     q3 = quat2N[3] * quat1N[3] - quat2N[0] * quat1N[0] - quat2N[1] * quat1N[1] - quat2N[2] * quat1N[2]
 
-    quat =  np.asarray([q0, q1, q2, q3])
+    quat = np.asarray([q0, q1, q2, q3])
     return quat / np.linalg.norm(quat)
+
 
 ########################################################################################################################
 def quatDiv(quat1, quat2):
     quat2Inv = -quat2
     quat2Inv[-1] *= -1
     return quatMult(quat1, quat2Inv)
+
 
 ########################################################################################################################
 def nlerp(quat1, quat2, inT):
@@ -494,6 +516,7 @@ def nlerp(quat1, quat2, inT):
 
     return outQuat
 
+
 ########################################################################################################################
 def quatRotateAngleAxis(axis, radians):
     axis = axis / np.linalg.norm(axis)
@@ -501,37 +524,46 @@ def quatRotateAngleAxis(axis, radians):
     quat = np.asarray([axis[0] * np.sin(halfRadians), axis[1] * np.sin(halfRadians), axis[2] * np.sin(halfRadians), np.cos(halfRadians)])
     return quat / np.linalg.norm(quat)
 
+
 ########################################################################################################################
 def quatRotateX(radians):
     return quatRotateAngleAxis([1,0,0], radians)
+
 
 ########################################################################################################################
 def quatRotateY(radians):
     return quatRotateAngleAxis([0,1,0], radians)
 
+
 ########################################################################################################################
 def quatRotateZ(radians):
     return quatRotateAngleAxis([0,0,1], radians)
+
 
 ########################################################################################################################
 def angleAxisRotation(axis, radians):
     return quat2dcm(quatRotateAngleAxis(axis, radians))
 
+
 ########################################################################################################################
 def rotateX(radians):
     return np.matrix([[1, 0, 0],[0, np.cos(radians), -np.sin(radians)],[0, np.sin(radians), np.cos(radians)]])
+
 
 ########################################################################################################################
 def rotateY(radians):
     return np.matrix([[np.cos(radians), 0, np.sin(radians)],[0, 1, 0],[-np.sin(radians), 0, np.cos(radians)]])
 
+
 ########################################################################################################################
 def rotateZ(radians):
     return np.matrix([[np.cos(radians), -np.sin(radians), 0],[np.sin(radians), np.cos(radians), 0],[0, 0, 1]])
 
+
 ########################################################################################################################
 def hat(xyz):
     return np.asarray([[0, -xyz[2], xyz[1]], [xyz[2], 0, -xyz[0]], [-xyz[1], xyz[0], 0]])
+
 
 ####################################################################################
 if __name__ == '__main__':