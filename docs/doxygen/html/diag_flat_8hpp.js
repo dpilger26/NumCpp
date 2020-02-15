2aa54f73a0582fb2f4d79713627f27d683a63fbf
David Pilger <dpilger26@gmail.com>
2020-02-15 00:48:17 -0700

    Fixed a typo in the diagonal() documentation

--- docs/doxygen/html/diag_flat_8hpp.js
+++ docs/doxygen/html/diag_flat_8hpp.js
@@ -1,4 +1,4 @@
 var diag_flat_8hpp =
 [
-    [ "diagflat", "diag_flat_8hpp.html#a5da8af5a46c4161d78bb68a8399cbaa8", null ]
+    [ "diagflat", "diag_flat_8hpp.html#a38e59bc5dd8d662f1d82448cc170817d", null ]
 ];
