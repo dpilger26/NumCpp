867be5d4ec39d3d63881bf52183d2dbcf8126074
David Pilger <dpilger26@gmail.com>
2020-03-15 22:49:52 -0600

    completed gaussNewton()

--- include/NumCpp/Linalg/gaussNewtonNlls.hpp
+++ include/NumCpp/Linalg/gaussNewtonNlls.hpp
@@ -31,13 +31,17 @@
 #pragma once
 
 #include "NumCpp/Core/Error.hpp"
+#include "NumCpp/Core/Shape.hpp"
 #include "NumCpp/Core/Types.hpp"
 #include "NumCpp/Core/TypeTraits.hpp"
+#include "NumCpp/Functions/rms.hpp"
+#include "NumCpp/Linalg/inv.hpp"
 #include "NumCpp/NdArray.hpp"
 
 #include <array>
-#include <string>
+#include <functional>
 #include <type_traits>
+#include <utility>
 
 namespace nc
 {
@@ -49,35 +53,83 @@ namespace nc
         /// It is a modification of Newton's method for finding a minimum of a function.
         /// https://en.wikipedia.org/wiki/Gauss%E2%80%93Newton_algorithm
         ///
-        /// @param      values: the values to be fitted
-        /// @param      initialGuess: the initial guess of the parameters to be solved for
-        /// @param      function: the function that is being fit
-        /// @param      derivatives: vector of std::functions to calculate the function 
-        ///                          derivatives   
         /// @param      numIterations: the number of iterations to perform, default 4.
+        /// @param      coordinates: the coordinate values.  The shape needs to be [n x d], where d is 
+        ///                     the number of diminsions of the fit function (f(x) is one dimensional, 
+        ///                     f(x, y) is two dimensions, etc), and n is the number of observations
+        ///                     that are being fit to.
+        /// @param      measurements: the measured values that are being fit
+        /// @param      function: a std::function of the function that is being fit. The function takes as 
+        ///                       inputs an NdArray of a single set of the coordinate values, and an NdArray 
+        ///                       of the current values of the fit parameters
+        /// @param      derivatives: array of std::functions to calculate the function 
+        ///                          derivatives. The function that is being fit. The function takes as 
+        ///                          inputs an NdArray of a single set of the coordinate values, and an NdArray 
+        ///                          of the current values of the fit parameters
+        /// @param      initialGuess: the initial guess of the parameters to be solved for
         ///
-        /// @return     NdArray of solved parameter values
+        /// @return     std::pair of NdArray of solved parameter values, and rms of the residuals value
         ///
-        template<typename dtype, typename ...Args,
+        template<typename dtype, typename ...Params,
             std::enable_if_t<std::is_arithmetic<dtype>::value, int> = 0,
-            std::enable_if_t<all_arithmetic<Args...>::value, int> = 0,
-            std::enable_if_t<all_same<dtype, Args...>::value, int> = 0
+            std::enable_if_t<all_arithmetic<Params...>::value, int> = 0,
+            std::enable_if_t<all_same<dtype, Params...>::value, int> = 0
         >
-        NdArray<double> gaussNewtonNlls(
-            const NdArray<dtype> values,
-            const std::array<dtype, sizeof...(Args)>& initialGuess,
-            const std::function<dtype(const NdArray<dtype>&, Args...)>& function,
-            const std::array<std::function<dtype(const NdArray<dtype>&, Args...)>, sizeof...(Args)>& derivatives,
-            const uint32 numIterations = 4)
+        std::pair<NdArray<double>, double> gaussNewtonNlls(
+            const uint32 numIterations, 
+            const NdArray<dtype>& coordinates,
+            const NdArray<dtype>& measurements,
+            const std::function<dtype(const NdArray<dtype>&, const NdArray<dtype>&)>& function,
+            const std::array<std::function<dtype(const NdArray<dtype>&, const NdArray<dtype>&)>, sizeof...(Params)>& derivatives,
+            Params... initialGuess)
         {
-            // need to get Args... values into an NdArray somehow???
+            const auto coordinatesShape = coordinates.shape();
 
-            for (uint32 i = 1; i <= numIterations; ++i)
+            if (coordinatesShape.rows != measurements.size())
             {
-                // build the jacobian and residials matrices and perform gauss-newton
+                THROW_INVALID_ARGUMENT_ERROR("coordinates number of rows, and measurements size must be the same.");
             }
 
-            return {};
+            NdArray<double> beta = NdArray<dtype>({initialGuess...}).template astype<double>().transpose();
+            NdArray<double> residuals(coordinatesShape.rows, 1);
+            NdArray<double> jacobian(coordinatesShape.rows, sizeof...(Params));
+
+            const auto colSlice = coordinates.cSlice();
+            for (uint32 iteration = 1; iteration <= numIterations; ++iteration)
+            {
+                for (uint32 measIdx = 0; measIdx < coordinatesShape.rows; ++measIdx)
+                {
+                    const auto coordinate = coordinates(measIdx, colSlice);
+
+                    residuals[measIdx] = static_cast<double>(measurements[measIdx]) - 
+                        static_cast<double>(function(coordinate, beta));
+
+                    for (uint32 paramIdx = 0; paramIdx < sizeof...(Params); ++paramIdx)
+                    {
+                        const auto& derivative = derivatives[paramIdx];
+                        jacobian(measIdx, paramIdx) = static_cast<double>(derivative(coordinate, beta));
+                    }
+                }
+
+                //std::cout << jacobian;
+                // perform the gauss-newton linear algebra
+                const auto jacobianT = jacobian.transpose();
+                const auto jacobianPsuedoInverse = linalg::inv(jacobianT.dot(jacobian));
+                const auto intermediate = jacobianPsuedoInverse.dot(jacobianT);
+                const auto deltaBeta = intermediate.dot(residuals);
+                beta += deltaBeta;
+            }
+
+            // calculate the final rms of the residuals
+            for (uint32 measIdx = 0; measIdx < coordinatesShape.rows; ++measIdx)
+            {
+                const auto coordinate = coordinates(measIdx, colSlice);
+
+                residuals[measIdx] = static_cast<double>(measurements[measIdx]) - 
+                    static_cast<double>(function(coordinate, beta));
+            }
+
+            return std::make_pair(beta.flatten(), rms(residuals).item());
         }
     }
 }
