b91aa7fb370268003b13e71bf3a064192eb92af4
David Pilger <dpilger26@gmail.com>
2020-07-21 21:55:54 -0600

    working on compiler test script for windows

--- test/compilers/test_compilers.py
+++ test/compilers/test_compilers.py
@@ -2,17 +2,30 @@ import argparse
 from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
-import pytest
-from pytest import ExitCode
+import platform
 import shutil
 import subprocess
 import time
 
+_IS_WINDOWS = False
+if platform.system() == 'Windows':
+    _IS_WINDOWS = True
+
+_DEFAULT_BOOST_DIR = None
+_DEFAULT_EIGEN_DIR = None
+_DEFAULT_OPENCV_DIR = None
+if _IS_WINDOWS:
+    _DEFAULT_BOOST_DIR = Path(r'C:\libs\boost\boost_1_73_0')
+    _DEFAULT_EIGEN_DIR = Path(r'C:\Program Files (x86)\Eigen3\share\eigen3\cmake')
+    _DEFAULT_OPENCV_DIR = Path(r'C:\libs\opencv-4.3.0\build\install')
+
 _NUMCPP_ROOT_DIR = (Path(__file__).parent / '..' / '..').resolve()
 _PYTEST_DIR = _NUMCPP_ROOT_DIR / 'test' / 'pytest'
-_UNIT_TEST_BIN_DIR = _NUMCPP_ROOT_DIR / 'test' / 'lib'
+_UNIT_TEST_LIB_DIR = _NUMCPP_ROOT_DIR / 'test' / 'lib'
 _EXAMPLE_DIR = _NUMCPP_ROOT_DIR / 'examples'
 _BUILD_DIR_NAME = 'build'
+if _IS_WINDOWS:
+    _BUILD_DIR_NAME += 'VS'
 
 _SUDO_REQUIRED = True
 try:
@@ -33,16 +46,25 @@ class SimpleTimer:
         print(f'{self._name} elapsed time: {time.perf_counter() - self._start:.1f} seconds')
 
 
-class Compiler(Enum):
-    GNU = 0
-    Clang = 1
+if _IS_WINDOWS:
+    class Compiler(Enum):
+        MSVC = 0
+        Default = MSVC
 
+    _COMPILERS = {Compiler.MSVC: 'msbuild'}
 
-_COMPILERS = {Compiler.GNU: 'gcc',
-              Compiler.Clang: 'clang'}
+    _COMPILER_VERSIONS = {Compiler.MSVC: ['v142']}  # DP NOTE: add v141 back in, need to use different boost version
+else:
+    class Compiler(Enum):
+        GNU = 0
+        Clang = 1
+        Default = GNU
 
-_COMPILER_VERSIONS = {Compiler.GNU: ['6', '7', '8', '9', '10'],
-                      Compiler.Clang: ['6.0', '7', '8', '9', '10']}
+    _COMPILERS = {Compiler.GNU: 'gcc',
+                  Compiler.Clang: 'clang'}
+
+    _COMPILER_VERSIONS = {Compiler.GNU: ['6', '7', '8', '9', '10'],
+                          Compiler.Clang: ['6.0', '7', '8', '9', '10']}
 
 
 class CxxStandard(Enum):
@@ -58,10 +80,10 @@ _CXX_STANDARDS = {CxxStandard.cxx_14: '14',
 
 @dataclass
 class BuildConfigs:
-    boost_root: str = None
-    eigen_root: str = None
-    opencv_root: str = None
-    cxx_standard: CxxStandard = None
+    boost_root: Path = _DEFAULT_BOOST_DIR
+    eigen_root: Path = _DEFAULT_EIGEN_DIR
+    opencv_root: Path = _DEFAULT_OPENCV_DIR
+    cxx_standard: CxxStandard = CxxStandard.cxx_17
 
 
 class Target(Enum):
@@ -70,6 +92,7 @@ class Target(Enum):
     InterfaceWithEigen = 2
     InterfaceWithOpenCV = 3
     ReadMe = 4
+    all = 5
 
 
 class Builder:
@@ -81,8 +104,8 @@ class Builder:
         self._cmake_configured = False
         self._current_compiler = None
         self._current_compiler_version = None
-        self.update_compiler(compiler=Compiler.GNU)
-        self.update_compiler_version(version=_COMPILER_VERSIONS[Compiler.GNU][-1])
+        self.update_compiler(compiler=Compiler.Default)
+        self.update_compiler_version(version=_COMPILER_VERSIONS[Compiler.Default][-1])
 
     def configure_cmake(self, build_configs: BuildConfigs = None):
         if self._build_dir.exists():
@@ -90,27 +113,42 @@ class Builder:
 
         self._build_dir.mkdir()
 
+        # NOTE: bypass InterfaceWithEigen example with VS on C++20 for now since Eigen doesn't
+        # seem to compile. Revisit this in the future
+        build_eigen_example = True
+        if _IS_WINDOWS and build_configs.cxx_standard == CxxStandard.cxx_20:
+            build_eigen_example = False
+
         cmake_cmd = ['cmake',
                      '-S', str(self._root_dir),
                      '-B', str(self._build_dir),
                      '-DNUMCPP_TEST=True',
-                     '-DNUMCPP_EXAMPLES=True']
+                     '-DNUMCPP_EXAMPLES=True',
+                     '-DEXAMPLE_GAUSS_NEWTON_NLLS=True',
+                     f'-DEXAMPLE_INTERFACE_WITH_EIGEN={build_eigen_example}',
+                     '-DEXAMPLE_INTERFACE_WITH_OPENCV=True',
+                     '-DEXAMPLE_README=True']
+
+        if _IS_WINDOWS:
+            cmake_cmd.extend(['-G', 'Visual Studio 16 2019',
+                              '-A', 'x64',
+                              f'-DCMAKE_GENERATOR_TOOLSET={self._current_compiler_version}'])
 
         if build_configs is not None:
             if build_configs.boost_root is not None:
-                if not Path(build_configs.boost_root).exists():
+                if not build_configs.boost_root.exists():
                     raise NotADirectoryError(f'Input boost_root directory does not exist:\n\t'
                                              f'{build_configs.boost_root}')
                 cmake_cmd.append(f'-DBOOSTROOT={build_configs.boost_root}')
 
-            if build_configs.eigen_root is not None:
-                if not Path(build_configs.eigen_root).exists():
+            if build_configs.eigen_root is not None and build_eigen_example:
+                if not build_configs.eigen_root.exists():
                     raise NotADirectoryError(f'Input eigen_root directory does not exist:\n\t'
                                              f'{build_configs.eigen_root}')
                 cmake_cmd.append(f'-DEigen3_DIR={build_configs.eigen_root}')
 
             if build_configs.opencv_root is not None:
-                if not Path(build_configs.opencv_root).exists():
+                if not build_configs.opencv_root.exists():
                     raise NotADirectoryError(f'Input opencv_root directory does not exist:\n\t'
                                              f'{build_configs.opencv_root}')
                 cmake_cmd.append(f'-DOpenCV_DIR={build_configs.opencv_root}')
@@ -127,13 +165,13 @@ class Builder:
 
         cmake_cmd = ['cmake',
                      '--build', str(self._build_dir),
-                     '--config', 'Release',
-                     '--target', target.name]
+                     '--config', 'Release']
+        if target != Target.all or not _IS_WINDOWS: 
+            cmake_cmd.extend(['--target', target.name])
         subprocess.check_call(cmake_cmd)
 
     def build_all(self):
-        self.build_unit_test()
-        self.build_examples()
+        self.build_target(Target.all)
 
     def build_unit_test(self):
         self.delete_unit_test()
@@ -164,12 +202,14 @@ class Builder:
         self.build_example_target(target=Target.ReadMe)
 
     def update_compiler(self, compiler: Compiler):
-        update_alternatives_cmd = ['update-alternatives',
-                                   '--set', 'cc', f'/usr/bin/{_COMPILERS[compiler]}']
-        if _SUDO_REQUIRED:
-            update_alternatives_cmd.insert(0, 'sudo')
+        if not _IS_WINDOWS:
+            update_alternatives_cmd = ['update-alternatives',
+                                       '--set', 'cc', f'/usr/bin/{_COMPILERS[compiler]}']
+            if _SUDO_REQUIRED:
+                update_alternatives_cmd.insert(0, 'sudo')
 
-        subprocess.check_call(update_alternatives_cmd)
+            subprocess.check_call(update_alternatives_cmd)
+
         self._current_compiler = compiler
 
     def update_compiler_version(self, version: str):
@@ -177,46 +217,59 @@ class Builder:
             raise ValueError(f"Unknown version '{version}' for {self._current_compiler} compiler\n"
                              f'Valid options are {_COMPILER_VERSIONS[self._current_compiler]}')
 
-        compiler_str = _COMPILERS[self._current_compiler]
-        update_alternatives_cmd = ['update-alternatives',
-                                   '--set', compiler_str, f'/usr/bin/{compiler_str}-{version}']
-        if _SUDO_REQUIRED:
-            update_alternatives_cmd.insert(0, 'sudo')
+        if not _IS_WINDOWS:
+            compiler_str = _COMPILERS[self._current_compiler]
+            update_alternatives_cmd = ['update-alternatives',
+                                       '--set', compiler_str, f'/usr/bin/{compiler_str}-{version}']
+            if _SUDO_REQUIRED:
+                update_alternatives_cmd.insert(0, 'sudo')
 
-        subprocess.check_call(update_alternatives_cmd)
+            subprocess.check_call(update_alternatives_cmd)
+
         self._current_compiler_version = version
 
     @staticmethod
     def delete_unit_test():
-        binary = _UNIT_TEST_BIN_DIR / f'{Target.NumCppPy.name}.so'
+        binary = _UNIT_TEST_LIB_DIR / f'{Target.NumCppPy.name}'
+        if _IS_WINDOWS:
+            binary = binary.with_suffix('.pyd')
+        else:
+            binary = binary.with_suffix('.so')
         if binary.is_file():
             binary.unlink()
 
     @staticmethod
     def delete_example(target: Target):
         target_str = target.name
-        output_file = _EXAMPLE_DIR / target_str / 'bin' / target_str
-        if output_file.is_file():
-            output_file.unlink()
+        binary = _EXAMPLE_DIR / target_str / 'bin' / target_str
+        if _IS_WINDOWS:
+            binary = binary.with_suffix('.exe')
+        if binary.is_file():
+            binary.unlink()
 
     @staticmethod
     def check_unit_test_binary():
-        binary = _UNIT_TEST_BIN_DIR / f'{Target.NumCppPy.name}.so'
+        binary = _UNIT_TEST_LIB_DIR / f'{Target.NumCppPy.name}'
+        if _IS_WINDOWS:
+            binary = binary.with_suffix('.pyd')
+        else:
+            binary = binary.with_suffix('.so')
         if not binary.is_file():
-            raise RuntimeError(f'unit test not successfully built')
+            raise RuntimeError(f'unit test not successfully built:\n\t{binary}')
 
     @staticmethod
     def check_example_binary(target: Target):
         target_str = target.name
         binary = _EXAMPLE_DIR / target_str / 'bin' / target_str
+        if _IS_WINDOWS:
+            binary = binary.with_suffix('.exe')
         if not binary.is_file():
-            raise RuntimeError(f'{target_str} target not successfully built')
+            raise RuntimeError(f'{target_str} target not successfully built:\n\t{binary}')
 
 
 def run_pytest(fileOrDirectory: str):
-    exit_code = pytest.main([fileOrDirectory])
-    if exit_code != ExitCode.OK:
-        raise RuntimeError('Pytest failures')
+    # spawn a seperate process to avoid this python instance from owning the dll/so
+    subprocess.check_call(['pytest', fileOrDirectory])
 
 
 def run_all(root_dir: str):
@@ -268,9 +321,13 @@ def run_single(root_dir: str,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--compiler', type=str, required=False, default='GNU')
-    parser.add_argument('-v', '--compiler_version', type=str, required=False, default='10')
-    parser.add_argument('-s', '--cxx_standard', type=str, required=False, default='17')
+    if _IS_WINDOWS:
+        parser.add_argument('-c', '--compiler', type=str, required=False, default='MSVC')
+        parser.add_argument('-v', '--compiler_version', type=str, required=False, default='v142')
+    else:
+        parser.add_argument('-c', '--compiler', type=str, required=False, default='GNU')
+        parser.add_argument('-v', '--compiler_version', type=str, required=False, default='10')
+    parser.add_argument('-s', '--cxx_standard', type=str, required=False, default='cxx_17')
     parser.add_argument('-t', '--target', type=str, required=False, default='all')
     parser.add_argument('-r', '--run_all', type=bool, required=False, default=False)
     args = parser.parse_args()
