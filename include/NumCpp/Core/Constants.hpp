c710330ac90fedbc4ff2fa1372cad6d6f778b285
David Pilger <dpilger26@gmail.com>
2022-07-11 21:04:51 -0600

    adding a github action config

--- include/NumCpp/Core/Constants.hpp
+++ include/NumCpp/Core/Constants.hpp
@@ -40,7 +40,7 @@ namespace nc
         constexpr double c   = 3.0e8;                                   ///< speed of light
         constexpr double e   = 2.718281828459045;                       ///< eulers number
         constexpr double inf = std::numeric_limits<double>::infinity(); ///< infinity
-        constexpr double pi  = 3.14159265358979323846;                  ///< Pi
+        constexpr double pi  = 3.141592653589793238462643383279502884;  ///< Pi
         const double     nan = std::nan("1");                           ///< NaN
         constexpr auto   j   = std::complex<double>(0, 1);              // sqrt(-1) unit imaginary number
 
