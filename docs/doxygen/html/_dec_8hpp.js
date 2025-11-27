5f8f6b89b472421dd4f90ebd0b5be15412276ad3
David Pilger <dpilger26@gmail.com>
2025-11-27 08:59:43 -0700

    Rc2.15.0 (fixed version) (#238)

--- docs/doxygen/html/_dec_8hpp.js
+++
@@ -1,8 +0,0 @@
-var _dec_8hpp =
-[
-    [ "Dec", "classnc_1_1coordinates_1_1_dec.html", "classnc_1_1coordinates_1_1_dec" ],
-    [ "Sign", "_dec_8hpp.html#a07a05f0462e5f74f05cbd04664c4ca94", [
-      [ "NEGATIVE", "_dec_8hpp.html#a07a05f0462e5f74f05cbd04664c4ca94a50546bf973283065b6ccf09faf7a580a", null ],
-      [ "POSITIVE", "_dec_8hpp.html#a07a05f0462e5f74f05cbd04664c4ca94aab6c31432785221bae58327ef5f6ea58", null ]
-    ] ]
-];
--- docs/doxygen/html/degree_seperation_8hpp.js
+++
@@ -1,5 +0,0 @@
-var degree_seperation_8hpp =
-[
-    [ "degreeSeperation", "degree_seperation_8hpp.html#a06135e21507cfe2aa1cb4154fe1702bf", null ],
-    [ "degreeSeperation", "degree_seperation_8hpp.html#abc47b2d64d107bcb19ff696ecff89edf", null ]
-];
