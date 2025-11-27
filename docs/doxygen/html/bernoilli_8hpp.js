5d41a135d67a8d0c7c0dcd753bd91098302caa28
David Pilger <dpilger26@gmail.com>
2025-11-27 08:51:55 -0700

    fixed version number

--- docs/doxygen/html/bernoilli_8hpp.js
+++
@@ -1,5 +0,0 @@
-var bernoilli_8hpp =
-[
-    [ "bernoulli", "bernoilli_8hpp.html#a8d4a1a62fc03a44cccfa4012413bd70f", null ],
-    [ "bernoulli", "bernoilli_8hpp.html#a1a5af4283601fd8663dcdc34599aede3", null ]
-];
--- docs/doxygen/html/radian_seperation_8hpp.js
+++
@@ -1,5 +0,0 @@
-var radian_seperation_8hpp =
-[
-    [ "radianSeperation", "radian_seperation_8hpp.html#aa009e573b47b51d34d3aae0b152566c8", null ],
-    [ "radianSeperation", "radian_seperation_8hpp.html#a712fd847123cfde7948c36ca59c6a435", null ]
-];
